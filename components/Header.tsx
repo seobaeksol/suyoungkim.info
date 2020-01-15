104d0b1fa0ee0392852804e1a7d23693b8fb20d3
Suyoung Kim <suyoung154@gmail.com>
2020-01-15 23:31:06 +0900

    Add header items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,13 @@
-const Header: React.FunctionComponent = () => <div>header</div>;
+
+
+const Header: React.FunctionComponent = () => (<div>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Follow</li>
+        <li>Contact</li>
+    </ul>
+</div>);
 
 export default Header;
