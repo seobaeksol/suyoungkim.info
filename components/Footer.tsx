62683d9c6b5184d083cc1b19cd4a161c3e8957fb
Suyoung Kim <suyoung154@gmail.com>
2020-01-19 23:22:53 +0900

    Adjust footer on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,43 +3,48 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Footer: React.FunctionComponent = () => (
   <div className="footer">
-    <h3 className="copyright">Copyright © 2020 Suyoung Kim</h3>
-    <span>
-      <a
-        href="mailto://suyoung154@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faEnvelope} size="2x" />
-      </a>
-    </span>
-    <span>
-      <a
-        href="https://www.facebook.com/dev.suyong"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img className="img__brand-logo" src="/images/logo_facebook.png" />
-      </a>
-    </span>
-    <span>
-      <a
-        href="https://www.instagram.com/suyoung_kr/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img className="img__brand-logo" src="/images/logo_instagram.png" />
-      </a>
-    </span>
-    <span>
-      <a
-        href="https://github.com/kimpeupeu"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img className="img__brand-logo" src="/images/logo_github_r.png" />
-      </a>
-    </span>
+    <div className="copyright__wrap">
+      <h3 className="copyright">Copyright © 2020 Suyoung Kim</h3>
+    </div>
+    <div className="brand__wrap">
+      <span>
+        <a
+          href="mailto://suyoung154@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://www.facebook.com/dev.suyong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="img__brand-logo" src="/images/logo_facebook.png" />
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://www.instagram.com/suyoung_kr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="img__brand-logo" src="/images/logo_instagram.png" />
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://github.com/kimpeupeu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="img__brand-logo" src="/images/logo_github_r.png" />
+        </a>
+      </span>
+    </div>
+
     <style jsx>
       {`
         .footer {
@@ -67,6 +72,7 @@ const Footer: React.FunctionComponent = () => (
         .copyright {
           margin-right: 15px;
           font-size: 1.2rem;
+          font-family: "Dancing Script", Arial;
         }
 
         .img__brand-logo {
@@ -77,6 +83,17 @@ const Footer: React.FunctionComponent = () => (
         span {
           margin-right: 10px;
           font-size: 0.8rem;
+        }
+
+        @media (max-width: 768px) {
+          .footer {
+            flex-direction: column;
+            justify-content: space-around;
+          }
+
+          .copyright {
+            font-size: 1.2rem;
+          }
         }
       `}
     </style>
